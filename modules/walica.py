a8dba3dd4364f8b17a0908ed17da1882813be45a
Tateshiki0529 <info@ttsk3.net>
2024-06-16 18:03:10 +0900

    v4.5.0-202406161802: '/add-event'コマンドの実装

---
+++ modules/walica.py
@@ -0,0 +1,42 @@
+from discord import (
+	Cog, Bot, ApplicationContext
+)
+from discord.ext.commands import slash_command as command
+from .functions import log
+from .constants import CONST_OTHERS
+
+from uuid import uuid4
+from json import dump
+
+class Walica(Cog):
+	def __init__(self, bot: Bot) -> None:
+		log('[Walica] Loading module "Walica"...')
+		self.bot: Bot = bot
+		log('[Walica] Module "Walica" loaded.')
+		return
+
+	# Command: /add-event
+	@command(
+		name = 'add-event',
+		description = '新しい割り勘イベントを作成します [Module: Walica]'
+	)
+	async def __add_event(self, ctx: ApplicationContext) -> None:
+		eventId: str = str(uuid4())
+		filepath: str = '%s/.event-queue/%s.json' % (CONST_OTHERS.WALICA_DIRECTORY, eventId)
+		data = {
+			'eventId': eventId,
+			'participants': [{
+				'name': u.display_name,
+				'icon': u.avatar.url,
+				'id': u.id,
+				'screenId': u.name
+			} for u in ctx.guild.members if not u.bot],
+			'returnTo': {
+				'guild': ctx.guild.id,
+				'channel': ctx.channel.id
+			}
+		}
+
+		with open(filepath, 'w') as fp: dump(data, fp)
+
+		await ctx.respond('%s/walica/create.php?eventId=%s' % (CONST_OTHERS.APP_URL, eventId))
